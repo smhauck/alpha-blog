d52a079ba827a7436bfff244fa002db2cbb90b11
William B. Hauck <wbhauck@yahoo.com>
2018-08-11 20:11:38 -0400

    create article and valideations

---
+++ app/models/article.rb
@@ -0,0 +1,5 @@
+class Article < ApplicationRecord::Base
+  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :description, presences: true, length: { minimum: 10, maximum: 300 }
+
+end
