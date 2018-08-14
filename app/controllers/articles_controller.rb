49be39b022e4a1c978091b43a3538c987eef7348
William B. Hauck <wbhauck@yahoo.com>
2018-08-13 22:13:49 -0400

    add styling to articles/show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = 'Article successfully created'
+      flash[:success] = 'Article successfully created'
       redirect_to article_path(@article)
     else
       render :new
@@ -13,7 +13,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
-    flash[:notice] = 'Article was successfully deleted'
+    flash[:danger] = 'Article was successfully deleted'
     redirect_to articles_path
   end
   
@@ -34,7 +34,7 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      flash[:notice] = 'Article successfully updated'
+      flash[:success] = 'Article successfully updated'
       redirect_to article_path(@article)
     else
       render :edit
