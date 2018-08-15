aeac25f2e7260fb7e637d48679e55bfd434c6995
William B. Hauck <wbhauck@yahoo.com>
2018-08-14 22:50:06 -0400

    adding user info to articles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
 
-  def gravatar_for(user)
+  def gravatar_for(user, size)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=150"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.username, class: 'img-circle')
   end
 
