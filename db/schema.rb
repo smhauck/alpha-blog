0fd21f7b9e0220c82343792975d56b76111aaf6a
William B. Hauck <wbhauck@yahoo.com>
2018-08-28 23:38:09 -0400

    added article_categories relationship / navigation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_19_030828) do
+ActiveRecord::Schema.define(version: 2018_08_29_030013) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
